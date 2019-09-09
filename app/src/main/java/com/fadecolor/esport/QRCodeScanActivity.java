7af4b77d52ba325db9863f2261a665599ca706ab
fade-color <318821221@qq.com>
2019-09-09 15:00:05 +0800

    增加二维码扫描，“发现、”我“的 界”面

---
+++ app/src/main/java/com/fadecolor/esport/QRCodeScanActivity.java
@@ -0,0 +1,14 @@
+package com.fadecolor.esport;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QRCodeScanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrcode_scan);
+    }
+}
